9fc47a72af47dd289ed109f89ca8240aa5e55e39
MicWes <micwesley@gmail.com>
2019-05-28 21:39:53 -0300

    delivery
    init TADSdelivery

---
+++ TADSdelivery/src/java/model/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Michael
+ */
+public class User {
+    private long id;
+    private String name;
+    private String cpf;
+    private String login;
+    private String adress;
+    private long city_id;
+}
